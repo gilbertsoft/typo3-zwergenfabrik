d3f44e4e75aef752c6835c7be90aad4a23c1f645
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2019-08-28 13:49:32 +0200

    [TASK] Increase last digit

--- packages/sitepackage/ext_emconf.php
+++ packages/sitepackage/ext_emconf.php
@@ -9,13 +9,13 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'misc',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.99.99',
+            'typo3' => '8.7.0-8.99.999',
             'gsfavicon' => '*',
-            'realurl' => '2.5.0-2.99.99',
-            'bootstrap_package' => '10.0.0-10.99.99',
-            'static_info_tables' => '6.7.0-6.99.99',
-            'go_maps_ext' => '2.6.0-2.99.99',
-            'aimeos' => '19.4.0-19.99.99'
+            'realurl' => '2.5.0-2.99.999',
+            'bootstrap_package' => '10.0.0-10.99.999',
+            'static_info_tables' => '6.7.0-6.99.999',
+            'go_maps_ext' => '2.6.0-2.99.999',
+            'aimeos' => '19.4.0-19.99.999'
         ],
         'conflicts' => [
         ],
