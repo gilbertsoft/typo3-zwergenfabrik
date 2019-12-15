5a547c2874c9d7ae11321dd22f44fad20d0ab80f
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2019-12-15 16:30:26 +0100

    [TASK] Restructure localconf

--- packages/sitepackage/ext_localconf.php
+++ packages/sitepackage/ext_localconf.php
@@ -1,28 +1,32 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-call_user_func(function($extensionKey) 
-{
-    /***************
-     * Add default configuration
+call_user_func(function () {
+    /**
+     * Predefines
      */
+    $extensionKey = 'gszwergenfabrik';
+
+    /**
+    * Add default configuration
+    */
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/PageTS/All.txt">'
     );
-
+    
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/UserTS/All.txt">'
     );
-
-    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets'][$extensionKey] = 
+    
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets'][$extensionKey] =
         'EXT:' . $extensionKey . '/Configuration/RTE/Default.yaml';
-
-    /***************
-     * Add Aimeos configuration
-     */
+    
+    /**
+    * Add Aimeos configuration
+    */
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['aimeos']['extDirs']['1_' . $extensionKey] =
         'EXT:' . $extensionKey . '/Resources/Private/Extensions/';
-
+    
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['aimeos']['confDirs']['1_' . $extensionKey] =
         'EXT:' . $extensionKey . '/Resources/Private/Config/';
-}, $_EXTKEY);
+});
