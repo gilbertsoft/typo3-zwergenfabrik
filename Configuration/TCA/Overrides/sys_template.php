3f93cb6bebaa003a95ddbe29652b892c1db2d537
Gilbertsoft <25326036+gilbertsoft@users.noreply.github.com>
2020-08-30 14:49:59 +0200

    [FEATURE] Introduce B2B shop

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,19 +1,23 @@
 <?php
+
 defined('TYPO3_MODE') || die();
 
-call_user_func(function()
-{
-    /**
-     * Temporary variables
-     */
-    $extensionKey = 'gszwergenfabrik';
-
+call_user_func(function ($extensionKey) {
     /**
      * Default TypoScript for Zwergenfabrik
      */
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         $extensionKey,
-        'Configuration/TypoScript',
-        'Zwergenfabrik'
+        'Configuration/TypoScript/Retail',
+        'Zwergenfabrik Retail'
     );
-});
+
+    /**
+     * Default TypoScript for Zwergenfabrik B2B
+     */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        $extensionKey,
+        'Configuration/TypoScript/Reseller',
+        'Zwergenfabrik Reseller'
+    );
+}, 'gszwergenfabrik');
